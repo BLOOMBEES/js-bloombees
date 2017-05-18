59b9ed963094070be46e1adc16370e8d21b79e09
am-adnbp <ApoloZeus99>
2017-05-18 19:55:11 +0200

    v1.1.2

--- src/js-bloombees.js
+++ src/js-bloombees.js
@@ -1,6 +1,6 @@
 Bloombees = new function () {
     // Config vars
-    this.version = '1.1.1';
+    this.version = '1.1.2';
     this.debug = false;
     this.apiUrl = Core.config.get('bloombeesApiUrl') || 'https://bloombees.com/h/api';
     this.oAuthUrl = Core.config.get('bloombeesOAuthUrl') || 'https://bloombees.com/h/service/oauth';
@@ -151,6 +151,7 @@ Bloombees = new function () {
         }
     }
 
+    // Change webKeys for the calls
     this.setWebKey = function (key){
         if(Bloombees.debug ) Core.log.printDebug('Bloombees.setWebKey("'+key+'")');
         if(typeof key != 'undefined') {
@@ -160,8 +161,48 @@ Bloombees = new function () {
         }
     }
 
+
+    // It opens a popup calling https://bloombees.com/h/service/oauth/{social}?ret=retUrl. The implementation of the retUrl
+    this.signInWithOauthPopUp = function (social, retUrl,callback) {
+
+        if((typeof social =='undefined') ) {
+            var response = {success:false, errors:["Bloombees.signInWithOauth(social,retUrl,callback) missing social=(facebook|google|instagram)"]};
+            if(typeof callback != 'undefined') callback(response);
+            return Bloombees.error('Bloombees.signInWithOauth(social,retUrl,callback) missing social=(facebook|google|instagram)');
+        }
+
+        if((typeof retUrl =='undefined') || !retUrl.indexOf("{id}")  ) {
+            var response = {success:false, errors:["Bloombees.signInWithOauth(social, retUrl,callback) missing a right retUrl included '?oauth_signin={id}' substring to subtitute it."]};
+            if(typeof callback != 'undefined') callback(response);
+            return Bloombees.error('Bloombees.signInWithOauth(social, retUrl,callback) missing a right retUrl included "?oauth_signin={id}" substring to subtitute it.');
+        }
+
+        Core.oauthpopup({
+            path: Core.config.get('bloombeesOAuthUrl')+'/'+social+'?ret='+retUrl,
+            callback: function()
+            {
+                var oauth_id = Core.cookies.get('oauth_signin');
+                if(oauth_id) {
+                    Core.cookies.remove('oauth_signin');
+                    Bloombees.oauth(oauth_id,function(response) {
+                        if(typeof callback != 'undefined') callback(response);
+                        else console.log(response);
+                    });
+                    //do callback stuff
+                } else {
+                    response = {success:false, errors:["missing cookie 'oauth_signin'. Apply in retUrl the JavaScriptCode to save the cookie oauth_signin"]};
+                    loader.removeClass('animated zoomOut active error');
+                    if(typeof callback != 'undefined') callback(response);
+                    else console.log(response);
+                }
+            }
+        });
+    };
+
+
+
     // Login with userpassword
-    this.login = function(data,callback) {
+    this.signInWithUserPassword = function(data,callback) {
         if(Bloombees.debug && !Core.debug) Core.log.printDebug('Bloombees.login calling: /auth/userpassword');
         Core.request.call({url:'/auth/userpassword',params:data,method:'POST'},function (response) {
             if(Core.user.isAuth()) Core.user.setAuth(false);
@@ -180,8 +221,6 @@ Bloombees = new function () {
             callback(response);
         });
     }
-
-
 
     // Login with userpassword
     this.signUp = function(data,callback) {
@@ -202,6 +241,14 @@ Bloombees = new function () {
             } else {
                 Bloombees.error('Bloombees.signUp',response);
             }
+            callback(response);
+        });
+    }
+
+    // Check if a code available for signup
+    this.signUpCodeAvailability = function(code,callback) {
+        if(Bloombees.debug && !Core.debug) Core.log.printDebug('Bloombees.signUpCodeAvailability calling: /register/availability/code/{code}');
+        Core.request.call({url:'/register/availability/code/'+code,method:'GET'},function (response) {
             callback(response);
         });
     }
@@ -232,26 +279,6 @@ Bloombees = new function () {
     }
 
     // Execute a logout
-    this.logout = function(callback) {
-        // Reset Bloombees Data
-        if(Core.user.isAuth()) {
-            if(Bloombees.debug && !Core.debug) Core.log.printDebug('Bloombees.logout calling: /auth/deactivate/dstoken');
-            Core.request.call({url:'/auth/deactivate/dstoken',method:'PUT'},function (response) {
-                if(response.success) {
-                    //---
-                    if(Bloombees.debug) console.log('Token deleted');
-                }
-                Core.user.setAuth(false);
-                Core.data.reset();
-                Core.request.token = '';
-                if(typeof callback != 'undefined') callback();
-            });
-        } else {
-            Core.data.reset();
-            Core.request.token = '';
-            if(typeof callback != 'undefined') callback();
-        }
-    }
 
     // Return the token from Cookies with name: this.cookieNameForToken
     this.getToken = function() {return(Core.cookies.get(Bloombees.cookieNameForToken) || '')};
@@ -328,6 +355,69 @@ Bloombees = new function () {
     }
 
     // Return current socialNetWorks
+    this.getHTMLVersionOfTermsAndConditions = function(version,callback,reload) {
+
+        if(typeof version != 'string') {
+            Bloombees.error('Bloombees.getHTMLVersionOfTermsAndConditions','version is not a string.');
+            callback('error loading terms.');
+            return;
+        }
+
+        var data = Core.data.get('getHTMLVersionOfTermsAndConditions');
+        if(typeof data == 'undefined' || reload) {
+            Core.data.set('getHTMLVersionOfTermsAndConditions_'+version,null);
+
+            Core.request.call({url:'/legal/tac/versions/'+version,method:'GET',responseType:'html'},function (response) {
+                Core.data.set('getHTMLVersionOfTermsAndConditions_'+version,response);
+                callback(response);
+            });
+        } else {
+            callback(data);
+        }
+    }
+
+    // connectUserSocialNetwork passing a oauth_id.. it requires
+    this.connectUserSocialNetwork = function (oauth_id,callback) {
+        if(!Bloombees.isAuth()) {
+            Bloombees.error('Bloombees.getUserSocialNetworks','user is not auth end.');
+            callback({success:false,error:['User is not auth in the frontend. Avoiding call']});
+            return;
+        }
+
+        Core.request.call({url:'/socialnetworks/connections/'+Core.user.get('User_id')+'/oauthservice',params:{id:oauth_id},method:'POST'},function (response) {
+            callback(response);
+        });
+
+    }
+
+    // disconnectUserSocialNetwork
+    this.disconnectUserSocialNetwork = function (social_id,callback) {
+        if(!Bloombees.isAuth()) {
+            Bloombees.error('Bloombees.getUserSocialNetworks','user is not auth end.');
+            callback({success:false,error:['User is not auth in the frontend. Avoiding call']});
+            return;
+        }
+        Core.request.call({url:'/socialnetworks/connections/'+Core.user.get('User_id')+'/'+social_id,method:'DELETE'},function (response) {
+            callback(response);
+        });
+    }
+
+    // callMeNow
+    this.callMeNow = function(data,callback) {
+
+        if(typeof data['Contact_name']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_name'));
+        if(typeof data['Contact_phone']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_phone'));
+        if(typeof data['Contact_sourceType']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_sourceType'));
+        if(typeof data['Contact_sourceSection']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_sourceSection'));
+        if(typeof data['Contact_lang']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_lang'));
+
+        Core.debug=true;
+        Core.request.call({url:'/forms/callmenow',method:'POST',params:data},function (response) {
+            callback(response);
+        });
+    }
+
+    // Return current socialNetWorks
     this.getUserTermsAndConditions = function(callback,reload) {
 
         if(!Bloombees.isAuth()) {
@@ -370,80 +460,7 @@ Bloombees = new function () {
         });
     }
 
-    // Return current socialNetWorks
-    this.getHTMLVersionOfTermsAndConditions = function(version,callback,reload) {
-
-        if(typeof version != 'string') {
-            Bloombees.error('Bloombees.getHTMLVersionOfTermsAndConditions','version is not a string.');
-            callback('error loading terms.');
-            return;
-        }
-
-        var data = Core.data.get('getHTMLVersionOfTermsAndConditions');
-        if(typeof data == 'undefined' || reload) {
-            Core.data.set('getHTMLVersionOfTermsAndConditions_'+version,null);
-
-            Core.request.call({url:'/legal/tac/versions/'+version,method:'GET',responseType:'html'},function (response) {
-                Core.data.set('getHTMLVersionOfTermsAndConditions_'+version,response);
-                callback(response);
-            });
-        } else {
-            callback(data);
-        }
-    }
-
-    this.connectUserSocialNetwork = function (oauth_id,callback) {
-        if(!Bloombees.isAuth()) {
-            Bloombees.error('Bloombees.getUserSocialNetworks','user is not auth end.');
-            callback({success:false,error:['User is not auth in the frontend. Avoiding call']});
-            return;
-        }
-
-        Core.request.call({url:'/socialnetworks/connections/'+Core.user.get('User_id')+'/oauthservice',params:{id:oauth_id},method:'POST'},function (response) {
-            callback(response);
-        });
-
-    }
-
-    this.disconnectUserSocialNetwork = function (social_id,callback) {
-        if(!Bloombees.isAuth()) {
-            Bloombees.error('Bloombees.getUserSocialNetworks','user is not auth end.');
-            callback({success:false,error:['User is not auth in the frontend. Avoiding call']});
-            return;
-        }
-        Core.request.call({url:'/socialnetworks/connections/'+Core.user.get('User_id')+'/'+social_id,method:'DELETE'},function (response) {
-            callback(response);
-        });
-
-
-    }
-
-    this.disconnectUserSocialNetwork = function (social_id,callback) {
-        if(!Bloombees.isAuth()) {
-            Bloombees.error('Bloombees.getUserSocialNetworks','user is not auth end.');
-            callback({success:false,error:['User is not auth in the frontend. Avoiding call']});
-            return;
-        }
-        Core.request.call({url:'/socialnetworks/connections/'+Core.user.get('User_id')+'/'+social_id,method:'DELETE'},function (response) {
-            callback(response);
-        });
-    }
-
-    // callMeNow
-    this.callMeNow = function(data,callback) {
-
-        if(typeof data['Contact_name']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_name'));
-        if(typeof data['Contact_phone']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_phone'));
-        if(typeof data['Contact_sourceType']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_sourceType'));
-        if(typeof data['Contact_sourceSection']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_sourceSection'));
-        if(typeof data['Contact_lang']=='undefined') return(Bloombees.error('Bloombees.callMeNow missing Contact_lang'));
-
-        Core.debug=true;
-        Core.request.call({url:'/forms/callmenow',method:'POST',params:data},function (response) {
-            callback(response);
-        });
-    }
-
+    // Manage errors associated to Bloombees
     this.error = function(title,content) {
         if(typeof title == 'string') title = '[Bloombees.error] '+title;
         if(typeof content != 'undefined')
